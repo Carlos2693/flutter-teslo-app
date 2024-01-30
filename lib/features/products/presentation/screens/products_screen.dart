1e1c5463eb2ca9abd62a1ae71e7b3e4e6dd61750
Carlos Buruel Ortiz <carlos.ob.26@gmail.com>
2024-01-30 11:00:36 -0600

    410. Pantalla de Producto

--- lib/features/products/presentation/screens/products_screen.dart
+++ lib/features/products/presentation/screens/products_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:go_router/go_router.dart';
 import 'package:teslo_shop/features/auth/presentation/widgets/widgets.dart';
 
 import 'package:teslo_shop/features/products/presentation/provider/provider.dart';
@@ -80,7 +81,10 @@ class _ProductsViewState extends ConsumerState {
         itemCount: productsState.products.length,
         itemBuilder: (context, index) {
           final product = productsState.products[index];
-          return ProductCard(product: product);
+          return GestureDetector(
+            onTap: () => context.push('/product/${product.id}'),
+            child: ProductCard(product: product)
+          );
         },
       ),
     );
