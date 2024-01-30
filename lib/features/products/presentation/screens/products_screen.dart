ca7122b3749cd3cecdfe8b9c276ea624dba25eda
Carlos Buruel Ortiz <carlos.ob.26@gmail.com>
2024-01-30 09:57:10 -0600

    408. Tarjetas de producto

--- lib/features/products/presentation/screens/products_screen.dart
+++ lib/features/products/presentation/screens/products_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:teslo_shop/features/auth/presentation/widgets/widgets.dart';
+
 import 'package:teslo_shop/features/products/presentation/provider/provider.dart';
 import 'package:teslo_shop/features/shared/shared.dart';
 
@@ -72,7 +74,7 @@ class _ProductsViewState extends ConsumerState {
         itemCount: productsState.products.length,
         itemBuilder: (context, index) {
           final product = productsState.products[index];
-          return Text(product.title);
+          return ProductCard(product: product);
         },
       ),
     );
